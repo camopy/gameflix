70d16d74ca958b68f743cffbe1e60f0d2237e117
Paulo Camopy <paulorenato.cpb@gmail.com>
2021-04-24 18:30:32 -0300

    feat: :sparkles: added login auth routes

--- gameflix.py
+++ gameflix.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session, flash
 from game import Game
 
 app = Flask(__name__)
+app.secret_key = "gameflix"
 
 tetris = Game("Tetris", "Arcade", "Megadrive")
 mario = Game("Super Mario", "Action", "SNES")
@@ -14,8 +15,40 @@ def index():
     return render_template("index.html", title="Gameflix", games=games)
 
 
+@app.route("/login")
+def login():
+    return render_template("login/login.html", title="Login")
+
+
+@app.route("/logout")
+def logout():
+    session["logged_user"] = None
+    flash("User not logged in")
+    return redirect("/")
+
+
+@app.route(
+    "/authenticate",
+    methods=[
+        "POST",
+    ],
+)
+def authenticate():
+    if "master" == request.form["password"]:
+        user = request.form["username"]
+        session["logged_user"] = user
+        flash(user + " has logged in")
+        return redirect("/")
+    else:
+        flash("Login failed, try again")
+        return redirect("/login")
+
+
 @app.route("/new_game")
 def new_game():
+    if not session["logged_user"]:
+        return redirect("/login")
+
     return render_template("game/new_game.html", title="New Game")
 
 
